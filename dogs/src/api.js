dbc58cbd8f4551b1af6b6017e11ccbf97a23feb3
Lucas Muniz Lara <lucasml@live.com>
2022-11-16 18:33:09 -0300

    external api calls and loginForm refactor

---
+++ dogs/src/api.js
@@ -0,0 +1,26 @@
+export const api_URL = "https://dogsapi.origamid.dev/json";
+
+export function TOKEN_POST(body) {
+  return {
+    url: api_URL + "/jwt-auth/v1/token",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function USER_GET(token) {
+  return {
+    url: api_URL + "/api/user",
+    options: {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    },
+  };
+}
